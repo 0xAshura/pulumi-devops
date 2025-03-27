b2fcefd9915ee17ef184a1f32ddc46f4d6357329
Mihir Limbad <151897310+0xAshura@users.noreply.github.com>
2025-03-26 20:14:59 -0400

    Update index.ts

--- infra/index.ts
+++ infra/index.ts
@@ -1,85 +1,61 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as synced_folder from "@pulumi/synced-folder";
-
-// Import the program's configuration settings.
-const config = new pulumi.Config();
-const path = config.get("path") || "./www";
-const indexDocument = config.get("indexDocument") || "index.html";
-const errorDocument = config.get("errorDocument") || "error.html";
-
-// Create an S3 bucket and configure it as a website.
-const bucket = new aws.s3.BucketV2("bucket");
-
-const bucketWebsite = new aws.s3.BucketWebsiteConfigurationV2("bucketWebsite", {
-    bucket: bucket.bucket,
-    indexDocument: {suffix: indexDocument},
-    errorDocument: {key: errorDocument},
-});
-
-// Configure ownership controls for the new S3 bucket
-const ownershipControls = new aws.s3.BucketOwnershipControls("ownership-controls", {
-    bucket: bucket.bucket,
-    rule: {
-        objectOwnership: "ObjectWriter",
+ 
+// Create the S3 bucket with website configuration
+const bucket = new aws.s3.Bucket("upadhyay-site", {
+    website: {
+        indexDocument: "index.html",
     },
 });
-
-// Configure public ACL block on the new S3 bucket
-const publicAccessBlock = new aws.s3.BucketPublicAccessBlock("public-access-block", {
-    bucket: bucket.bucket,
+ 
+// Upload the index.html file to the bucket
+new aws.s3.BucketObject("index", {
+    bucket: bucket,
+    content: "<html><h1>Hello, Nilay from Pulumi!</h1></html>",
+    key: "index.html",
+    contentType: "text/html",
+});
+ 
+// Create a public access block (so CloudFront can access the files)
+new aws.s3.BucketPublicAccessBlock("bucket-public-access-block", {
+    bucket: bucket.id,
     blockPublicAcls: false,
+    ignorePublicAcls: false,
+    blockPublicPolicy: false,
+    restrictPublicBuckets: false,
 });
-
-// Use a synced folder to manage the files of the website.
-const bucketFolder = new synced_folder.S3BucketFolder("bucket-folder", {
-    path: path,
-    bucketName: bucket.bucket,
-    acl: "public-read",
-}, { dependsOn: [ownershipControls, publicAccessBlock]});
-
-// Create a CloudFront CDN to distribute and cache the website.
-const cdn = new aws.cloudfront.Distribution("cdn", {
-    enabled: true,
-    origins: [{
-        originId: bucket.arn,
-        domainName: bucketWebsite.websiteEndpoint,
-        customOriginConfig: {
-            originProtocolPolicy: "http-only",
-            httpPort: 80,
-            httpsPort: 443,
-            originSslProtocols: ["TLSv1.2"],
-        },
-    }],
-    defaultCacheBehavior: {
-        targetOriginId: bucket.arn,
-        viewerProtocolPolicy: "redirect-to-https",
-        allowedMethods: [
-            "GET",
-            "HEAD",
-            "OPTIONS",
-        ],
-        cachedMethods: [
-            "GET",
-            "HEAD",
-            "OPTIONS",
-        ],
-        defaultTtl: 600,
-        maxTtl: 600,
-        minTtl: 600,
-        forwardedValues: {
-            queryString: true,
-            cookies: {
-                forward: "all",
+ 
+// Apply a public read-only bucket policy
+new aws.s3.BucketPolicy("bucket-policy", {
+    bucket: bucket.id,
+    policy: bucket.id.apply(bucketId => JSON.stringify({
+        Version: "2012-10-17",
+        Statement: [
+            {
+                Effect: "Allow",
+                Principal: "*",
+                Action: "s3:GetObject",
+                Resource: `arn:aws:s3:::${bucketId}/*`,
             },
-        },
-    },
-    priceClass: "PriceClass_100",
-    customErrorResponses: [{
-        errorCode: 404,
-        responseCode: 404,
-        responsePagePath: `/${errorDocument}`,
+        ],
+    })),
+});
+ 
+// Create the CloudFront distribution
+const cdn = new aws.cloudfront.Distribution("cdn", {
+    origins: [{
+        domainName: bucket.bucketRegionalDomainName,
+        originId: bucket.arn,
     }],
+    enabled: true,
+    defaultRootObject: "index.html",
+    defaultCacheBehavior: {
+        viewerProtocolPolicy: "redirect-to-https",
+        allowedMethods: ["GET", "HEAD"],
+        cachedMethods: ["GET", "HEAD"],
+        targetOriginId: bucket.arn,
+        forwardedValues: { queryString: false, cookies: { forward: "none" } },
+    },
     restrictions: {
         geoRestriction: {
             restrictionType: "none",
@@ -89,9 +65,7 @@ const cdn = new aws.cloudfront.Distribution("cdn", {
         cloudfrontDefaultCertificate: true,
     },
 });
-
-// Export the URLs and hostnames of the bucket and distribution.
-export const originURL = pulumi.interpolate`http://${bucketWebsite.websiteEndpoint}`;
-export const originHostname = bucketWebsite.websiteEndpoint;
-export const cdnURL = pulumi.interpolate`https://${cdn.domainName}`;
-export const cdnHostname = cdn.domainName;
+ 
+// Export the S3 bucket name and CloudFront URL
+export const bucketName = bucket.bucket;
+export const cloudFrontUrl = cdn.domainName;
